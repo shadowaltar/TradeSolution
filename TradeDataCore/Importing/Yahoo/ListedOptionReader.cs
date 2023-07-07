4f0150dacac994c543b39263282743e6381be3dc
Shadow <shadowaltar@gmail.com>
2023-07-07 08:31:45 +0800

    c8

---
+++ TradeDataCore/Importing/Yahoo/ListedOptionReader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradeDataCore.Importing.Yahoo
+{
+    public class ListedOptionReader
+    {
+        public static async Task ReadUnderlyingSummary()
+        {
+            const string url = @"https://query1.finance.yahoo.com/v7/finance/options/{0}";
+        }
+    }
+}
--- TradePort/Controllers/PriceController.cs
+++ TradePort/Controllers/PriceController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using TradeDataCore.Database;
 using TradeDataCore.Essentials;
+using TradeDataCore.Importing.Yahoo;
 using TradeDataCore.StaticData;
 using TradeDataCore.Utils;
 
@@ -68,6 +69,36 @@ public class PriceController : Controller
     }
 
     /// <summary>
+    /// Get prices given exchange, code, interval and start time.
+    /// </summary>
+    /// <param name="exchange"></param>
+    /// <param name="code"></param>
+    /// <param name="intervalStr"></param>
+    /// <param name="rangeStr"></param>
+    /// <returns></returns>
+    [HttpGet("yahoo/{exchange}/{code}")]
+    public async Task<ActionResult> GetPriceFromYahoo(string exchange = "HKEX",
+        string code = "00001",
+        [FromQuery(Name = "interval")] string intervalStr = "1d",
+        [FromQuery(Name = "range")] string rangeStr = "10y")
+    {
+        if (intervalStr.IsBlank())
+            return BadRequest("Invalid interval string.");
+        var interval = IntervalTypeConverter.Parse(intervalStr);
+        if (interval == IntervalType.Unknown)
+            return BadRequest("Invalid interval string.");
+        if (rangeStr == null)
+            return BadRequest("Invalid range string.");
+        var range = TimeRangeTypeConverter.Parse(rangeStr);
+        if (range == TimeRangeType.Unknown)
+            return BadRequest("Invalid range string.");
+
+        var symbol = Identifiers.ToYahooSymbol(exchange, code);
+        var prices = new PriceReader().ReadYahooPrices(new List<string> { symbol }, interval, range);
+        return Ok(prices);
+    }
+
+    /// <summary>
     /// VERY HEAVY CALL!
     /// Gets all security price data from Yahoo in this exchange and save to database.
     /// </summary>
@@ -96,13 +127,50 @@ public class PriceController : Controller
 
         foreach (var (ticker, security) in tickers)
         {
-            if (allPrices.TryGetValue(ticker, out var prices))
+            if (allPrices.TryGetValue(ticker, out var tuple))
             {
-                await Storage.InsertPrices(security.Id, intervalStr, prices);
+                await Storage.InsertPrices(security.Id, intervalStr, tuple.Prices);
                 var count = await Storage.Execute("SELECT COUNT(Interval) FROM " + DatabaseNames.PriceTable, DatabaseNames.MarketData);
-                Console.WriteLine($"Code {security.Code} exchange {security.Exchange} (Yahoo {ticker}) price count: {prices.Count}/{count}");
+                Console.WriteLine($"Code {security.Code} exchange {security.Exchange} (Yahoo {ticker}) price count: {tuple.Prices.Count}/{count}");
             }
         }
-        return Ok(allPrices.ToDictionary(p => p.Key, p => p.Value.Count));
+        return Ok(allPrices.ToDictionary(p => p.Key, p => p.Value.Prices.Count));
+    }
+
+    /// <summary>
+    /// VERY HEAVY CALL!
+    /// Gets all security price data from Yahoo in this exchange and save to database.
+    /// </summary>
+    /// <param name="exchange"></param>
+    /// <param name="intervalStr"></param>
+    /// <param name="rangeStr"></param>
+    /// <returns></returns>
+    [HttpGet("{exchange}/get-and-save-one")]
+    public async Task<ActionResult> GetAndSaveHongKongOne(
+        string exchange = "HKEX",
+        string code = "00001",
+        [FromQuery(Name = "interval")] string intervalStr = "1h",
+        [FromQuery(Name = "range")] string rangeStr = "10y")
+    {
+        var security = await Storage.ReadSecurity(exchange, code);
+        var tickers = new Dictionary<string, Security>();
+        tickers[Identifiers.ToYahooSymbol(security.Code, security.Exchange)] = security;
+
+        var interval = IntervalTypeConverter.Parse(intervalStr);
+        var range = TimeRangeTypeConverter.Parse(rangeStr);
+        var priceReader = new PriceReader();
+        var allPrices = await priceReader.ReadYahooPrices(tickers.Select(p => p.Key).ToList(),
+            interval, range);
+
+        foreach (var (ticker, sec) in tickers)
+        {
+            if (allPrices.TryGetValue(ticker, out var tuple))
+            {
+                await Storage.InsertPrices(sec.Id, intervalStr, tuple.Prices);
+                var count = await Storage.Execute("SELECT COUNT(Interval) FROM " + DatabaseNames.PriceTable, DatabaseNames.MarketData);
+                Console.WriteLine($"Code {sec.Code} exchange {sec.Exchange} (Yahoo {ticker}) price count: {tuple.Prices.Count}/{count}");
+            }
+        }
+        return Ok(allPrices.ToDictionary(p => p.Key, p => p.Value.Prices.Count));
     }
 }
