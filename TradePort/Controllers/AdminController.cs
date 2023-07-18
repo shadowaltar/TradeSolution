644147f11a4696dc0080e1c73185fe9b4a756b90
shadowaltar <shadowaltar@gmail.com>
2023-07-18 23:59:05 +0800

    c15

--- TradePort/Controllers/AdminController.cs
+++ TradePort/Controllers/AdminController.cs
@@ -45,29 +45,58 @@ public class AdminController : Controller
     /// <summary>
     /// WARNING, this will erase all the data. Rebuild all the tables.
     /// </summary>
+    /// <param name="password">Mandatory</param>
+    /// <param name="intervalStr">Must be used along with <paramref name="secTypeStr"/>. Only supports <see cref="IntervalType.OneMinute"/> / <see cref="IntervalType.OneHour"/>, Only supports <see cref="IntervalType.OneDay"/>.</param>
+    /// <param name="secTypeStr">Must be used along with <paramref name="intervalStr"/>.</param>
     /// <returns></returns>
     [HttpGet("rebuild-price-tables")]
-    public async Task<ActionResult> RebuildPriceTables([FromQuery(Name = "password")] string password)
+    public async Task<ActionResult> RebuildPriceTables([FromQuery(Name = "password")] string password,
+        [FromQuery(Name = "interval")] string? intervalStr,
+        [FromQuery(Name = "sec-type")] string? secTypeStr)
     {
         if (password.IsBlank()) return BadRequest();
         if (!Credential.IsPasswordCorrect(password)) return BadRequest();
 
-        var tuples = new (string table, string db, IntervalType interval, SecurityType secType)[]
+        IntervalType interval = IntervalType.Unknown;
+        SecurityType secType = SecurityType.Unknown;
+        if (intervalStr != null)
+            interval = IntervalTypeConverter.Parse(intervalStr);
+        if (secTypeStr != null)
+            secType = SecurityTypeConverter.Parse(secTypeStr);
+
+        if (interval == IntervalType.Unknown || secType == SecurityType.Unknown)
         {
-            (DatabaseNames.StockPrice1mTable, DatabaseNames.MarketData, IntervalType.OneMinute, SecurityType.Equity),
-            (DatabaseNames.StockPrice1hTable, DatabaseNames.MarketData, IntervalType.OneHour, SecurityType.Equity),
-            (DatabaseNames.StockPrice1dTable, DatabaseNames.MarketData, IntervalType.OneDay, SecurityType.Equity),
-            (DatabaseNames.FxPrice1mTable, DatabaseNames.MarketData, IntervalType.OneMinute, SecurityType.Fx),
-            (DatabaseNames.FxPrice1hTable, DatabaseNames.MarketData, IntervalType.OneHour, SecurityType.Fx),
-            (DatabaseNames.FxPrice1dTable, DatabaseNames.MarketData, IntervalType.OneDay, SecurityType.Fx),
-        };
-        var results = await Task.WhenAll(tuples.Select(async t =>
+            return BadRequest("Either both interval and sec-type parameters are not set, or both must be presented.");
+        }
+
+        if (interval == IntervalType.Unknown)
         {
-            var (table, db, interval, secType) = t;
+            var tuples = new (string table, string db, IntervalType interval, SecurityType secType)[]
+            {
+                (DatabaseNames.StockPrice1mTable, DatabaseNames.MarketData, IntervalType.OneMinute, SecurityType.Equity),
+                (DatabaseNames.StockPrice1hTable, DatabaseNames.MarketData, IntervalType.OneHour, SecurityType.Equity),
+                (DatabaseNames.StockPrice1dTable, DatabaseNames.MarketData, IntervalType.OneDay, SecurityType.Equity),
+                (DatabaseNames.FxPrice1mTable, DatabaseNames.MarketData, IntervalType.OneMinute, SecurityType.Fx),
+                (DatabaseNames.FxPrice1hTable, DatabaseNames.MarketData, IntervalType.OneHour, SecurityType.Fx),
+                (DatabaseNames.FxPrice1dTable, DatabaseNames.MarketData, IntervalType.OneDay, SecurityType.Fx),
+            };
+            var results = await Task.WhenAll(tuples.Select(async t =>
+            {
+                var (table, db, interval, secType) = t;
+                await Storage.CreatePriceTable(interval, secType);
+                var r = await Storage.CheckTableExists(table, db);
+                return (table, r);
+            }));
+            return Ok(results.ToDictionary(p => p.table, p => p.r));
+        }
+        else if (interval is IntervalType.OneMinute or IntervalType.OneHour or IntervalType.OneDay)
+        {
+            var table = DatabaseNames.GetPriceTableName(interval, secType);
             await Storage.CreatePriceTable(interval, secType);
-            var r = await Storage.CheckTableExists(table, db);
-            return (table, r);
-        }));
-        return Ok(results.ToDictionary(p => p.table, p => p.r));
+            var r = await Storage.CheckTableExists(table, DatabaseNames.MarketData);
+            return Ok(new Dictionary<string, bool> { { table, r } });
+        }
+
+        return BadRequest($"Invalid parameter combination: {intervalStr}, {secTypeStr}");
     }
 }
